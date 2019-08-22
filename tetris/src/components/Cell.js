f7b6a6e4c68e3a26de2e6a2577ec91f6e757ee96
ChanhoKim <cksgh405@gmail.com>
2019-08-22 15:07:53 +0900

    컴포넌트 스타일링

---
+++ tetris/src/components/Cell.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyledCell } from './Styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
+
+const Cell = ({ type }) => (
+    <StyledCell type={type} color={TETROMINOS[type].color} />
+);
+
+
+export default Cell;
