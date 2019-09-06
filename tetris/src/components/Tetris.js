ae3d19a311da501235908d8a4ea8aea612aabf9b
ChanhoKim <cksgh405@gmail.com>
2019-09-06 13:25:36 +0900

    충돌 조건 및 한줄 완성 시 삭제기능 완성 ----남은 기능---- 점수 및 레벨

--- tetris/src/components/Tetris.js
+++ tetris/src/components/Tetris.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 
-import { createStage } from '../gameHelpers';
+import { createStage, checkCollision } from '../gameHelpers';
 
 //styled components
 import { StyledTetrisWrapper, StyledTetris } from './Styles/StyledTetris';
 
 //Custom Hooks
+import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
 
@@ -20,23 +21,45 @@ const Tetris = () => {
     const [dropTime, setDropTime] = useState(null);
     const [gameOver, setGameOver] = useState(null);
 
-    const [player, updatePlayerPos, resetPlayer] = usePlayer();
-    const [stage, setStage] = useStage(player);
+    const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
+    const [stage, setStage] = useStage(player, resetPlayer);
 
     const movePlayer = dir => {
-        updatePlayerPos({ x: dir, y: 0 });
+        if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+            updatePlayerPos({ x: dir, y: 0 });
+        }
     }
 
     const startGame = () => {
         setStage(createStage());
+        setDropTime(1000);
         resetPlayer();
+        setGameOver(false);
     }
 
     const drop = () => {
-        updatePlayerPos({ x: 0, y: 1, collided: false });
+        if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+            updatePlayerPos({ x: 0, y: 1, collided: false });
+        } else {
+            if (player.pos.y < 1) {
+                console.log("GAME OVER!!");
+                setGameOver(true);
+                setDropTime(null);
+            }
+            updatePlayerPos({ x: 0, y: 0, collided: true })
+        }
+    }
+
+    const keyUp = ({ keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 40) {
+                setDropTime(1000);
+            }
+        }
     }
 
     const dropPlayer = () => {
+        setDropTime(null);
         drop();
     }
 
@@ -48,12 +71,23 @@ const Tetris = () => {
                 movePlayer(1);
             } else if (keyCode === 40) {    //down arrow
                 dropPlayer();
+            } else if (keyCode === 38) {    //up arrow
+                playerRotate(stage, 1);
             }
         }
     }
 
+    useInterval(() => {
+        drop();
+    }, dropTime)
+
     return (
-        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+        <StyledTetrisWrapper
+            role="button"
+            tabIndex="0"
+            onKeyDown={e => move(e)}
+            onKeyUp={keyUp}
+        >
             <StyledTetris>
                 <Stage stage={stage} />
                 <aside>
